265100148ddefbbfc13b5331ce90a98d591e0d1f
DarknessUptime <140251064+DarknessUptime@users.noreply.github.com>
2023-07-22 16:19:24 +0300

    Create parsher.js

---
+++ parsher.js
@@ -0,0 +1,59 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const chalk = require("chalk");
+const moment = require("moment");
+var Jimp = require("jimp");
+const { Client, Util } = require("discord.js");
+const fs = require("fs");
+const replaceOnce = require("replace-once");
+require("./util/eventLoader.js")(client);
+const db = require("quick.db");
+const queue = new Map();
+const { Canvas } = require("canvas-constructor");
+const YouTube = require("simple-youtube-api");
+const superagent = require("superagent");
+const ytdl = require("ytdl-core");
+const ayarlar = require("./ayarlar.json");
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
